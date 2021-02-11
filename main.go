38b05eb5532b0ebf4ca1f54a50bd4867310aba4e
urvashigupta7 <urvashi072000@gmail.com>
2021-02-11 19:57:33 +0530

    add database

--- main.go
+++ main.go
@@ -5,17 +5,28 @@ import (
     "log"
     "net/http"
     "github.com/gorilla/mux"
+    "database/sql"
+    _ "github.com/go-sql-driver/mysql"
 )
 
+var db *sql.DB
+var err error
 
 func homePage(w http.ResponseWriter, r *http.Request){
-   randomString := []string{
-       "Learn React",
-       "Use Typescript with React",
-       "Have Fun!!",
-       "Use emoji's",
-       "Look at memes",
-   }
+   var randomString [5]string
+   todos, err := db.Query("SELECT * FROM test")
+    if err != nil {
+        panic(err.Error())
+    }
+    for todos.Next(){
+        var id int
+        var todo string
+        err = todos.Scan(&id,&todo)
+        if err != nil {
+            panic(err.Error())
+        }
+        randomString[id]=todo
+    }
    json.NewEncoder(w).Encode(randomString)
 }
 
@@ -26,5 +37,30 @@ func handleRequests() {
 }
 
 func main() {
+    db,err= sql.Open("mysql","urgupta:urgupta@tcp(mysqldb:3306)/test")
+    if err != nil {
+        panic(err.Error())
+    }
+    defer db.Close()
+    _,err = db.Exec("CREATE TABLE IF NOT EXISTS test ( id integer, todo varchar(52) )")
+    if err != nil {
+       panic(err)
+    }
+    randomString := []string{
+        "Learn React",
+        "Use Typescript with React",
+        "Have Fun!!",
+        "Use emoji's",
+        "Look at memes",
+    }
+    for id, todo := range randomString {
+        insert,err := db.Prepare("INSERT INTO test(id, todo) VALUES(?,?)")
+        if err != nil {
+            panic(err.Error())
+        }
+        insert.Exec(id,todo)
+        log.Println("todo: " + todo)
+    }
+    
     handleRequests()
 }
