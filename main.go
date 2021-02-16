3f9ae61ccaa3a0b95a09ca3fa74a14f4c12c6491
urvashigupta7 <urvashi072000@gmail.com>
2021-02-16 20:34:29 +0530

    add post todo route

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "fmt"
     "encoding/json"
     "log"
     "net/http"
@@ -11,23 +12,54 @@ import (
 
 var db *sql.DB
 var err error
-
+type test_struct struct {
+    Todo string
+}
 func homePage(w http.ResponseWriter, r *http.Request){
-   var randomString [5]string
-   todos, err := db.Query("SELECT * FROM test")
+
+switch r.Method {
+case "GET":
+    numberOfTodos,err := db.Query("SELECT COUNT(*) AS nos FROM test")
     if err != nil {
         panic(err.Error())
     }
-    for todos.Next(){
-        var id int
-        var todo string
-        err = todos.Scan(&id,&todo)
-        if err != nil {
-            panic(err.Error())
-        }
-        randomString[id]=todo
+    var nos int
+    numberOfTodos.Next()
+    err = numberOfTodos.Scan(&nos)
+    if err != nil {
+        panic(err.Error())
     }
-   json.NewEncoder(w).Encode(randomString)
+    randomString := make([]string, nos+1)
+    todos, err := db.Query("SELECT * FROM test")
+     if err != nil {
+         panic(err.Error())
+     }
+     for todos.Next(){
+         var id int
+         var todo string
+         err = todos.Scan(&id,&todo)
+         if err != nil {
+             panic(err.Error())
+         }
+         randomString[id]=todo
+     }
+    json.NewEncoder(w).Encode(randomString)
+case "POST":
+   decoder := json.NewDecoder(r.Body)
+   var t test_struct
+   err := decoder.Decode(&t)
+   if err != nil {
+    panic(err.Error())
+   }
+   insert,err := db.Prepare("INSERT INTO test(todo) VALUES(?)")
+    if err != nil {
+        panic(err.Error())
+    }
+    insert.Exec(t.Todo)
+    fmt.Println(t.Todo)
+default:
+    fmt.Println("only get and post request supported")
+}
 }
 
 func handleRequests() {
@@ -42,25 +74,15 @@ func main() {
         panic(err.Error())
     }
     defer db.Close()
-    _,err = db.Exec("CREATE TABLE IF NOT EXISTS test ( id integer, todo varchar(52) )")
+    _,err = db.Exec("CREATE TABLE IF NOT EXISTS test ( id integer NOT NULL AUTO_INCREMENT, todo varchar(52),  PRIMARY KEY (id) )")
     if err != nil {
        panic(err)
     }
-    randomString := []string{
-        "Learn React",
-        "Use Typescript with React",
-        "Have Fun!!",
-        "Use emoji's",
-        "Look at memes",
-    }
-    for id, todo := range randomString {
-        insert,err := db.Prepare("INSERT INTO test(id, todo) VALUES(?,?)")
-        if err != nil {
-            panic(err.Error())
-        }
-        insert.Exec(id,todo)
-        log.Println("todo: " + todo)
-    }
-    
     handleRequests()
 }
+
+// "Learn React",
+// "Use Typescript with React",
+// "Have Fun!!",
+// "Use emoji's",
+// "Look at memes",
